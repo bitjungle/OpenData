80872bdedd92371d17dcf444e27c96944c11f3d1
Rune Mathisen <devel@bitjungle.com>
2021-10-22 08:48:57 +0200

    More data

--- Skien_weather_met.no/retrieve_data.py
+++ Skien_weather_met.no/retrieve_data.py
@@ -9,10 +9,11 @@ See http://www.gnu.org/licenses/gpl-3.0.html
 import requests
 import pandas as pd
 import datetime
+from os.path import exists
 
 # Specify year span for data retrieval
 year_start = 2014
-year_stop = 2020
+year_stop = 2021
 
 # Enter your own client ID
 # https://frost.met.no/auth/requestCredentials.html
@@ -54,43 +55,47 @@ for year in range(year_start, year_stop + 1):
 
         # Output csv file name
         file_name = '{}/{}-{:02}.csv'.format(output_folder, year, month)
-        print('Writing data to {}'.format(file_name))
-
-        # Define endpoint and parameters
-        # Time specification: https://frost.met.no/concepts2.html#timespecifications
-        endpoint = 'https://frost.met.no/observations/v0.jsonld'
-        parameters = {
-            'sources': data_sources,
-            'elements': data_elements,
-            'referencetime': ref_time,
-        }
-        # Issue an HTTP GET request
-        print('Requesting data from frost.met.no...')
-        r = requests.get(endpoint, parameters, auth=(client_id,''))
-        # Extract JSON data
-        json = r.json()
-
-        # Check if the request worked, print out any errors
-        if r.status_code == 200:
-            data = json['data']
-            print('...data retrieved from frost.met.no')
+        if exists(file_name):
+            # We already have data for this month
+            print(file_name, 'exists already')
         else:
-            print('Error! Returned status code %s' % r.status_code)
-            print('Message: %s' % json['error']['message'])
-            print('Reason: %s' % json['error']['reason'])
-            break
+            print('Writing data to {}'.format(file_name))
 
-        # This will return a Dataframe with all of the observations in a table format
-        print('Constructing dataframe...')
-        df = pd.DataFrame()
-        for i in range(len(data)):
-            row = pd.DataFrame(data[i]['observations'])
-            row['referenceTime'] = data[i]['referenceTime'].replace('.000Z', '').replace('T', ' ')
-            row['sourceId'] = data[i]['sourceId']
-            df = df.append(row)
-        df = df.reset_index()
-        print('...done')
+            # Define endpoint and parameters
+            # Time specification: https://frost.met.no/concepts2.html#timespecifications
+            endpoint = 'https://frost.met.no/observations/v0.jsonld'
+            parameters = {
+                'sources': data_sources,
+                'elements': data_elements,
+                'referencetime': ref_time,
+            }
+            # Issue an HTTP GET request
+            print('Requesting data from frost.met.no...')
+            r = requests.get(endpoint, parameters, auth=(client_id,''))
+            # Extract JSON data
+            json = r.json()
 
-        print('Writing to file...')
-        df.to_csv(file_name)
-        print('...done')
+            # Check if the request worked, print out any errors
+            if r.status_code == 200:
+                data = json['data']
+                print('...data retrieved from frost.met.no')
+            else:
+                print('Error! Returned status code %s' % r.status_code)
+                print('Message: %s' % json['error']['message'])
+                print('Reason: %s' % json['error']['reason'])
+                break
+
+            # This will return a Dataframe with all of the observations in a table format
+            print('Constructing dataframe...')
+            df = pd.DataFrame()
+            for i in range(len(data)):
+                row = pd.DataFrame(data[i]['observations'])
+                row['referenceTime'] = data[i]['referenceTime'].replace('.000Z', '').replace('T', ' ')
+                row['sourceId'] = data[i]['sourceId']
+                df = df.append(row)
+            df = df.reset_index()
+            print('...done')
+
+            print('Writing to file...')
+            df.to_csv(file_name)
+            print('...done')
